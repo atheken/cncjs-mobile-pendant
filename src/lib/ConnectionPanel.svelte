69d1e33b0a22d96fc410af6ae0c25ad8effb0c59
Andrew Theken <andrew@attr.io>
2023-02-28 16:28:54 -0500

    Controller can now connect to a serial port.

--- src/lib/ConnectionPanel.svelte
+++ src/lib/ConnectionPanel.svelte
@@ -6,22 +6,31 @@
     .available {
         color: green;
     }
-
 </style>
 
 <script lang="ts">
-    import type { Controller } from "./Controller";
+    import type { Controller, SerialPort } from "./Controller";
+    
     export let model:Controller;
 
-    let ports = model.ports
-    
+    let {ports, active_port} = model;
+
+    let selected:SerialPort;
 </script>
 
 <div>
-    <select>
-        {#each $ports as p(p.port)}
-        <option class:inuse={p.inuse} class:available={!p.inuse}>{p.port}</option>
-        {/each}
-    </select>
-    <button on:click={()=> model.refresh_serial_list()}>Refresh</button>
+    <form on:submit|preventDefault={()=>{}}>
+        {#if $active_port?.port }
+            Currently connected to: <span>{$active_port.port}</span>
+            <button on:click={()=> model.close_connection() }>Disconnect</button>
+        {:else}
+            <select bind:value={selected}>
+                {#each $ports as p(p.port)}
+                <option class:inuse={p.inuse} class:available={!p.inuse} value={p}>{p.port}</option>
+                {/each}
+            </select>
+            <button disabled={!selected} on:click={()=> model.open_connection(selected)}>Connect</button>
+            <button on:click={()=> model.refresh_serial_list() }>Refresh</button>
+        {/if}
+    </form>
 </div>
--- src/lib/Controller.ts
+++ src/lib/Controller.ts
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
-import { writable, type Writable } from "svelte/store";
+import { writable, get } from "svelte/store";
+import { ulid } from 'ulid';
 
 export class Coordinate {
     X:number
@@ -26,17 +27,45 @@ export class StartupEvent {
     ports: SerialPort[]
 }
 
+export class CommandRecord{
+    id: string
+    mtime: number
+    enabled: boolean
+    title: string
+    commands: string
+}
+
+export class CommandQueryResult {
+    records: CommandRecord[] = []
+}
+
+export class SigninResult {
+    enabled:boolean
+    token: string
+    name: string
+}
+
 export class Controller {
+    close_connection(): any {
+        this.socket.emit("close", get(this.active_port)?.port)
+    }
+    open_connection(selected: SerialPort) {
+        this.socket.emit("open", selected.port, { controllerType : "Grbl", "baudrate" : 115200 })    
+    }
+
+    private controller_id = ulid();
 
     socket: io.socket;
     token : Promise<string>;
     ports = writable<SerialPort[]>([])
+    active_port = writable<SerialPort>(null)
 
     constructor(){
         this.configure()
     }
 
     async configure(){
+        
         this.token = new Promise(async (resolve, reject)=>{
             try{
             let token = null;
@@ -65,12 +94,36 @@ export class Controller {
         });
         this.socket.emit("probe");
         this.refresh_serial_list();
+
+
+        // dump all packets to the debug console for inspection.
+        this.socket.on("connection", function(packet, next){
+            console.debug(packet);
+            next();
+        });
+
+        this.socket.on("serialport:open", f => {
+             this.active_port.set(f);
+             this.write("$$");
+        });
+
+        this.socket.on("serialport:close", () => {
+            this.active_port.set(null);
+            this.refresh_serial_list();
+        });
+
+        this.socket.on("serialport:read", s=> {console.debug(s) })
+
         this.socket.connect();
     }
 
     refresh_serial_list() {
         this.socket.emit("list");
-        console.info("requested serial list update");
+    }
+
+    write(command:string, appendNewline: boolean = false){
+        let write = appendNewline ? "writeln" : "write";
+        this.socket.emit(write, get(this.active_port).port, command, { _sender_ : this.controller_id });
     }
 
     async commands() : Promise<CommandQueryResult> {
@@ -85,22 +138,4 @@ export class Controller {
             throw err;
         }
     }
-}
-
-export class CommandRecord{
-    id: string
-    mtime: number
-    enabled: boolean
-    title: string
-    commands: string
-}
-
-export class CommandQueryResult {
-    records: CommandRecord[] = []
-}
-
-export class SigninResult {
-    enabled:boolean
-    token: string
-    name: string
 }
