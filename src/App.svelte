5c595055c50025af4cb6658a08ff2b601e425a2b
Andrew Theken <andrew@attr.io>
2023-03-03 12:27:46 +0000

    Display a more appropriate error message when something bubbles, and fix api paths.

--- src/App.svelte
+++ src/App.svelte
@@ -1,83 +1,84 @@
 <script lang="ts">
-  import { Controller, SerialPort } from './lib/Controller';
-  import StandardControls from './lib/StandardControls.svelte';
-  import JogControls from './lib/JogControls.svelte';
-  import JobStatus from './lib/JobStatus.svelte';
-  import { onMount } from 'svelte';
-  import ConnectionPanel from './lib/ConnectionPanel.svelte';
-  import DisconnectControl from './lib/DisconnectControl.svelte';
-  import Icon from './lib/Icon.svelte';
+	import { Controller, SerialPort } from './lib/Controller';
+	import StandardControls from './lib/StandardControls.svelte';
+	import JogControls from './lib/JogControls.svelte';
+	import JobStatus from './lib/JobStatus.svelte';
+	import { onMount } from 'svelte';
+	import ConnectionPanel from './lib/ConnectionPanel.svelte';
+	import DisconnectControl from './lib/DisconnectControl.svelte';
+	import Icon from './lib/Icon.svelte';
 
-  let controller: Controller;
-  let active_port;
-  let tabs = [
-    {
-      id: 'commands',
-      alt: 'Session Commands',
-      icon: 'server'
-    },
-    {
-      id: 'job',
-      alt: 'Job Control',
-      icon: 'play'
-    },
-    {
-      id: 'connection',
-      alt: 'Connection Status',
-      icon: 'swap'
-    },
-    {
-      id: 'jog',
-      alt: 'Jog Machine',
-      icon: 'move-alt'
-    }
-  ];
-  let selected_tab_id = 'commands';
-  let error;
+	let controller: Controller;
+	let active_port;
+	let tabs = [
+		{
+			id: 'commands',
+			alt: 'Session Commands',
+			icon: 'server'
+		},
+		{
+			id: 'job',
+			alt: 'Job Control',
+			icon: 'play'
+		},
+		{
+			id: 'connection',
+			alt: 'Connection Status',
+			icon: 'swap'
+		},
+		{
+			id: 'jog',
+			alt: 'Jog Machine',
+			icon: 'move-alt'
+		}
+	];
+	let selected_tab_id = 'commands';
+	let error;
 
-  onMount(async () => {
-    try {
-      controller = await Controller.Initialize();
-      active_port = controller.active_port;
-    } catch (err) {
-      error = err;
-    }
-  });
+	onMount(async () => {
+		try {
+			controller = await Controller.Initialize();
+			active_port = controller.active_port;
+		} catch (err) {
+			error = err;
+			console.error(err);
+		}
+	});
 </script>
 
 <div class="container mx-auto">
-  {#if !controller}
-    {#if error}
-      The following error: {error}
-    {:else}
-      <div class="text-center align-middle">Mobile Pendant is Loading</div>
-    {/if}
-  {:else}
-    <ConnectionPanel model={controller} />
-    <div class={!$active_port ? 'blur-sm' : ''}>
-      {#if selected_tab_id == 'commands'}
-        <StandardControls model={controller} />
-      {:else if selected_tab_id == 'job'}
-        <JobStatus model={controller} />
-      {:else if selected_tab_id == 'jog'}
-        <JogControls />
-      {:else if selected_tab_id == 'connection'}
-        <DisconnectControl model={controller} />
-      {/if}
-      <div class="btm-nav btm-nav-sm ">
-        {#each tabs as t (t.id)}
-          <button
-            class="bg-primary"
-            on:click={() => (selected_tab_id = t.id)}
-            class:active={t.id == selected_tab_id}
-            class:text-accent={t.id == selected_tab_id}
-            data-tab-selection="t.id"
-            ><Icon icon={t.icon} />
-          </button>
-        {/each}
-      </div>
-    </div>
-  {/if}
+	{#if !controller}
+		{#if error}
+			<div class="bg-error align-middle text-error">><Icon icon="warn" />The app is currently unavailable.</div>
+		{:else}
+			<div class="text-center align-middle">Mobile Pendant is Loading</div>
+		{/if}
+	{:else}
+		<ConnectionPanel model={controller} />
+		<div class={!$active_port ? 'blur-sm' : ''}>
+			{#if selected_tab_id == 'commands'}
+				<StandardControls model={controller} />
+			{:else if selected_tab_id == 'job'}
+				<JobStatus model={controller} />
+			{:else if selected_tab_id == 'jog'}
+				<JogControls />
+			{:else if selected_tab_id == 'connection'}
+				<DisconnectControl model={controller} />
+			{/if}
+			<div class="btm-nav btm-nav-sm ">
+				{#each tabs as t (t.id)}
+					<button
+						class="bg-primary"
+						on:click={() => (selected_tab_id = t.id)}
+						class:active={t.id == selected_tab_id}
+						class:text-accent={t.id == selected_tab_id}
+						data-tab-selection="t.id"
+						><Icon icon={t.icon} />
+					</button>
+				{/each}
+			</div>
+		</div>
+	{/if}
 </div>
 
 <style>
