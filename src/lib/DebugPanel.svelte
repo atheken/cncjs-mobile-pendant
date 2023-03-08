2d60980bc1ce7d21bd3b684945b68d020a5d1d14
Andrew Theken <andrew@attr.io>
2023-03-08 23:06:44 +0000

    Improve design of debug screen.

--- src/lib/DebugPanel.svelte
+++ src/lib/DebugPanel.svelte
@@ -10,11 +10,11 @@
 <div>
 	<Divider>Debug</Divider>
 	<div class="flex flex-row place-content-center space-x-1">
-		<button class="btn btn-sm basis-[45%]" on:click={() => model.cncjs_command('statusreport')}>Status report</button>
-		<button class="btn btn-sm basis-[45%]" on:click={() => model.cncjs_command('probe')}>probe</button>
+		<button class="btn-sm btn basis-[45%]" on:click={() => model.cncjs_command('statusreport')}>Status report</button>
+		<button class="btn-sm btn basis-[45%]" on:click={() => model.cncjs_command('probe')}>probe</button>
 	</div>
 	{#key $controller}
-		<Divider>Controllers:</Divider>
+		<Divider>Controller:</Divider>
 		<div class="text-center text-xs text-slate-700">Updated: {new Date()}</div>
 		<pre>{JSON.stringify($controller, null, ' ')}</pre>
 	{/key}
