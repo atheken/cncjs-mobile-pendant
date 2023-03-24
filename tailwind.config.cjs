c3bdaa362cc14fff1dc59365279e53ff3d068f3d
Andrew Theken <andrew@attr.io>
2023-03-24 22:25:40 +0000

    Make the navigation elements stand out a bit from the content area.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@ module.exports = {
   content: ['./src/**/*.{svelte,js,ts}'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
   theme: {
+    extend : {
+      dropShadow: {
+        'toolbar': '0 5px 5px rgba(0, 0, 0, 0.5)',
+      }
+    },
     container: {
       center: true
     },
