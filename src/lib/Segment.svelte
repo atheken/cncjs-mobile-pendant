10661d63e09c2e24881fe0ce4622c5000c1b24ff
Andrew Theken <andrew@attr.io>
2023-03-26 15:21:28 +0000

    Add border directly to segment components.

--- src/lib/Segment.svelte
+++ src/lib/Segment.svelte
@@ -7,12 +7,12 @@
 </script>
 
 <span
-	class="inline-block divide-x divide-blue-500 rounded-md border border-blue-500 {classes ||
+	class="inline-block divide-x divide-blue-500 rounded-md text-sm {classes ||
 		''}">
 	{#each elements as a}
 		<button
 			on:click|preventDefault={() => (element = a)}
-			class="min-w-[2rem] p-1 text-sm first:rounded-l-md last:rounded-r-md focus:relative"
+			class="min-w-[2rem] border-t border-b border-blue-500 p-1 first:rounded-l-md first:border-l last:rounded-r-md last:border-r focus:relative"
 			class:bg-blue-500={element == a}
 			class:text-blue-500={element != a}
 			class:text-white={element == a}>
